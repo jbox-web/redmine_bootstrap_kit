7b9db0edbe8dc8e9f563197eae9a523227bc7c03
Nicolas <nrodriguez@jbox-web.com>
2014-04-19 16:16:33 +0200

    Init repository

---
+++ init.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+require 'redmine'
+
+Redmine::Plugin.register :redmine_bootstrap_kit do
+  name 'Redmine Bootstrap Kit plugin'
+  author 'Nicolas Rodriguez'
+  description 'This is a Bootstrap Kit plugin for Redmine'
+  version '0.1'
+  url 'https://github.com/jbox-web/redmine_bootstrap_kit'
+  author_url 'https://github.com/jbox-web'
+end
