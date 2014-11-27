49b292e39b940b051b7283ca9f2eb2cb877aa464
root <root@dev.jbox-web.fr>
2014-11-27 19:31:13 +0100

    Minor change

--- assets/javascripts/bootstrap.js
+++ assets/javascripts/bootstrap.js
@@ -49,21 +49,19 @@ function addAlertMessage(object){
 BootstrapTooltips
 */
 function setBootstrapToolTips(){
-  $('.tooltips').each(function(index, element) {
-    $(element).tooltip({
-      position: {
-        my: "left+15 left",
-        at: "right center",
-        using: function( position, feedback ) {
-          $(this).css(position);
-          $('<div>')
-            .addClass( 'arrow left' )
-            .addClass( feedback.vertical )
-            .addClass( feedback.horizontal )
-            .appendTo( this );
-        }
+  $('.tooltips').tooltip({
+    position: {
+      my: "left+15 left",
+      at: "right center",
+      using: function( position, feedback ) {
+        $(this).css(position);
+        $('<div>')
+          .addClass( 'arrow left' )
+          .addClass( feedback.vertical )
+          .addClass( feedback.horizontal )
+          .appendTo( this );
       }
-    });
+    }
   });
 }
 
