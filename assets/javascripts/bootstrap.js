44038e32bc5ea1eda3b0558d6c266248d1a289a2
root <root@dev.jbox-web.fr>
2015-03-14 13:10:50 +0100

    Add ZeroClipboard plugin with helper method

--- assets/javascripts/bootstrap.js
+++ assets/javascripts/bootstrap.js
@@ -190,3 +190,23 @@ function setTagIt(){
     $(this).tagit(tagit_options);
   });
 }
+
+
+function setZeroClipBoard(element){
+  var client = new ZeroClipboard($(element));
+
+  client.on('ready', function() {
+    $('#global-zeroclipboard-html-bridge').tooltip({
+      title:     $(element).data('label-to-copy'),
+      placement: 'right'
+    });
+
+    client.on('beforecopy', function() {
+      $('#global-zeroclipboard-html-bridge').tooltip('show');
+    });
+
+    client.on('aftercopy', function() {
+      $('.tooltip .tooltip-inner').text($(element).data('label-copied'));
+    });
+  });
+}
