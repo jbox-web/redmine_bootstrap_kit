8f8ad7181625a647d0343cc53b24ddcff5341ced
root <root@dev.jbox-web.fr>
2015-03-14 11:36:30 +0100

    Add RedminePresenterHelper

---
+++ app/helpers/redmine_presenter_helper.rb
@@ -0,0 +1,10 @@
+module RedminePresenterHelper
+
+  def present(object, klass = nil, *args)
+    klass ||= "#{object.class.base_class}Presenter".constantize
+    presenter = klass.new(object, self, *args)
+    yield presenter if block_given?
+    presenter
+  end
+
+end
