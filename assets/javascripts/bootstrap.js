dad5142d16a13bcf6095deeb55f87b669dfe1fe7
root <root@dev.jbox-web.fr>
2015-03-14 14:36:51 +0100

    Add SetSortableElement helper method

--- assets/javascripts/bootstrap.js
+++ assets/javascripts/bootstrap.js
@@ -210,3 +210,23 @@ function setZeroClipBoard(element){
     });
   });
 }
+
+
+// Return a helper with preserved width of cells
+var fixHelper = function(e, ui) {
+  ui.children().each(function() {
+    $(this).width($(this).width());
+  });
+  return ui;
+};
+
+
+function setSortableElement(element, form) {
+  $(element).sortable({
+    helper: fixHelper,
+    axis: 'y',
+    update: function(event, ui) {
+      $.post($(form).data('update-url'), $(this).sortable('serialize'));
+    }
+  });
+}
