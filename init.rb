3d1ccfeb5b42ff1efb03a25be15f35516bdeefb7
root <root@dev.jbox-web.fr>
2015-03-14 02:41:45 +0100

    Bump to 0.2.2 version

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_bootstrap_kit do
   name 'Redmine Bootstrap Kit plugin'
   author 'Nicolas Rodriguez'
   description 'This is a Bootstrap Kit plugin for Redmine'
-  version '0.2.1'
+  version '0.2.2'
   url 'https://github.com/jbox-web/redmine_bootstrap_kit'
   author_url 'https://github.com/jbox-web'
 end
