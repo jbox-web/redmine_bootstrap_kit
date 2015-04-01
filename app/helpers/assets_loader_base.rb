ac5d97a065d06d20df5d0111126b5f476b16e414
root <root@dev.jbox-web.fr>
2015-04-01 16:55:44 +0200

    Minor change

--- app/helpers/assets_loader_base.rb
+++ app/helpers/assets_loader_base.rb
@@ -1,12 +1,12 @@
 module AssetsLoaderBase
 
   def rbk_include_js(js)
-    javascript_include_tag(js, plugin: 'redmine_bootstrap_kit')
+    javascript_include_tag(js, plugin: 'redmine_bootstrap_kit') + "\n"
   end
 
 
   def rbk_include_css(css)
-    stylesheet_link_tag(css, plugin: 'redmine_bootstrap_kit')
+    stylesheet_link_tag(css, plugin: 'redmine_bootstrap_kit') + "\n"
   end
 
 end
